f7854b415292bf2352ba1a26f3d842d660bd9804
yinweixing <yinweixing@i-games.com.cn>
2022-09-13 18:34:17 +0800

    changes

--- readfiles/main.py
+++ readfiles/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import time
 
 
 def io_tools(read_filepath, write_filepath, mode):
@@ -17,12 +18,18 @@ def main():
     now = datetime.now()
     date = now.strftime("%Y_%m_%d_%H_%M_%S")
     filename = f"G:/pythontest/{date}.txt"
-    rangeOver = input("重复几次：")
-    for i in range(0, int(rangeOver)):
-        filename = f"G:/pythontest/{date}_{i}.txt"
-        io_tools(r'G:\pythontest\test1.txt', filename, 1)
+    #rangeOver = input("重复几次：")
+    #for i in range(0, int(rangeOver)):
+    t_end = time.time() + 1
+    i = 0
+    while time.time() < t_end:
+            filename = f"F:/pythontest/{date}_{i}.txt"
+            io_tools(r'F:\ForTest\temp1.txt', filename, 1)
+            i = i+1
 
 
-print(datetime.now())
+
+
 main()
-print(datetime.now())
+
+
