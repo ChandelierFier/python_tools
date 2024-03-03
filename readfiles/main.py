6669a5b8f8d5ea7a8775b7e5960e20922c0aecee
Fierneiya <chandelier.fier@gmail.com>
2024-03-03 14:05:26 +0800

    remove old code

--- readfiles/main.py
+++ readfiles/main.py
@@ -44,15 +44,6 @@ def main():
     now = datetime.now()
     date = now.strftime("%Y_%m_%d_%H_%M_%S")
     filename = f"G:/pythontest/{date}.txt"
-    #rangeOver = input("重复几次：")
-    #for i in range(0, int(rangeOver)):
-    t_end = time.time() + 1
-    i = 0
-    while time.time() < t_end:
-            filename = f"F:/pythontest/{date}_{i}.txt"
-            io_tools(r'F:\ForTest\temp1.txt', filename, 1)
-            i = i+1
-
     # rangeOver = input("重复几次：")
     # for i in range(0, int(rangeOver)):
     #    filename = f"G:/pythontest/{date}_{i}.txt"
@@ -61,5 +52,6 @@ def main():
 
 print(datetime.now())
 main()
+print(datetime.now())
 
 
